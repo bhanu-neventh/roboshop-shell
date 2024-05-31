26fa78c3974989cc979f02cd79b69fb4e1188001
bhanu-neventh <bhanuneventh@gmail.com>
2024-05-31 17:52:18 +0530

    Roboshop-shell--scripts

---
+++ mongodb.sh
@@ -0,0 +1,50 @@
+#/bin/bash
+
+ID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+TIMESTAMP=$(date +%F)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "script started execting at $TIMESTAMP" &>> $LOGFILE
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ...... $R FAILED $N"
+    else
+        echo -e "$2 ...... $G SUCCESS $N"
+    fi
+}
+
+
+if [ $ID -ne 0 ]
+then 
+    echo -e "$R ERROR :: Please run this script with root access $N"
+    exit 1
+else
+    echo "you are root user"
+fi
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
+VALIDATE $? "Copied MongoDB Repo"
+
+dnf install mongodb-org -y &>> $LOGFILE
+VALIDATE $? "Installing MongoDB"
+
+systemctl enable mongod &>> $LOGFILE
+VALIDATE $? "Enabling MongoDB"
+
+systemctl start mongod &>> $LOGFILE
+VALIDATE $? "Starting MongoDB"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf &>> $LOGFILE
+VALIDATE $? "Remote access to MongoDB"
+
+systemctl restart mongod &>> $LOGFILE
+VALIDATE $? "Restarting MongoDB"
+
